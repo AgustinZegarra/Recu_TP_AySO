933714b2b035057702569d33c8a68cb9ae479bdb
Agustin Zegarra <agustinzegarra49@gmail.com>
2025-06-03 00:30:44 +0000

    ADD: Punto C - Usuarios y permisos parte 2

---
+++ Punto_C.sh
@@ -0,0 +1,8 @@
+sudo groupadd p1c1_2024_Todos
+sudo useradd -m -g p1c1_2024_Todos p1c1_2024_u2
+echo "p1c1_2024_u2:clave1" | sudo chpasswd
+sudo usermod -aG p1c1_2024_Todos p1c1_2024_u1
+sudo chgrp -R p1c1_2024_Todos /datos
+sudo chmod -R 770 /datos
+sudo chmod o+x /datos
+sudo su -c "id >> /datos/validar1.txt" p1c1_2024_u2
